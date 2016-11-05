1eef9fc5d4f2bbc004a394fac6ccf5b13a9444b3
Cassandra Cruz <celkamada@gmail.com>
2016-11-05 15:50:27 -0700

    Add Rakefile with test task

--- test/test_reductio.rb
+++ test/test_reductio.rb
@@ -1,19 +1,18 @@
 require "minitest/autorun"
+require "minitest/test"
 require_relative "../lib/reductio"
-
-R = Reductio
 
 class ReductioTest < Minitest::Test
   def test_it_defines_something
-    assert R, 'Reductio should exist'
+    assert Reductio, 'Reductio should exist'
   end
 
   def test_add_does_what_we_expect
-    assert_equal R::Add.(1, 1), 2, '1 + 1 should equal 2'
+    assert_equal Reductio::Add.(1, 1), 2, '1 + 1 should equal 2'
   end
 
   def test_add_is_curried
-    inc = R::Add.(1)
+    inc = Reductio::Add.(1)
     assert_equal inc.(1), 2, '1 incremented should equal 2'
   end
 
